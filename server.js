7924642a3e2c69630b90543e4e5d5aeb72dfe391
Deja_Gordon <dejagordon80@GMAIL.COM>
2023-02-06 12:24:46 +0100

    Add Database Connectiion Setup

--- server.js
+++ server.js
@@ -2,13 +2,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Rewuire DB Configuration File
+const db = require('./config/db')
+
+// Establish Database Connection
+mongoose.connect(db, { useNewUrlParser: true});
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'))
+
 //Require Route Files
 const indexRouter = require('./routes/index');
 
-// Instantiate ExpressApp Object
+// Instantiate Express Application Object
 const app = express();
-
-
 
 //Define Port for the API to run on
 const port = process.env.PORT || 5001;
@@ -25,4 +30,3 @@ app.use(indexRouter);
 // Start the server and listen for requests on the given port
 app.listen(port, () => console.log(`blogy is listening on port ${port}`))
 
-//INDEX Route
